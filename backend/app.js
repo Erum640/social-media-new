1d9a8b58636cbbe484f3702c913558ab3222e54c
ERUM FATIMA <101186928+Erum640@users.noreply.github.com>
2023-12-21 13:20:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // const io = socketIo(server);
 const io = require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://social-media-frontend-s1fc.onrender.com",
       methods: ["GET", "PUT"],
       allowedHeaders: ["my-custom-header"],
       credentials: true
